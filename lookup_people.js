380da228bb4396038c2b2fadb50c70ca9b5408f8
Ryo <kanadajinlee@yahoo.ca>
2016-11-22 19:26:32 +0000

    The file now finds people by first/last name when you input a name after node lookup_people.js

---
+++ lookup_people.js
@@ -0,0 +1,28 @@
+//access HEROKU's "famous_people" table
+const person = process.argv[2]
+
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  let string = `SELECT * FROM famous_people WHERE last_name = '${person}' OR first_name = '${person}'`
+  client.query(string, [], (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    console.log(result.rows); //output: 1
+    client.end();
+  });
+});
