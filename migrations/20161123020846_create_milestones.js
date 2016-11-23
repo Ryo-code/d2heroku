012965628b160030de8177f5003e497d37aea6b5
Ryo <kanadajinlee@yahoo.ca>
2016-11-23 03:46:07 +0000

    Created the foreign key and synced it up with the famous people tables

--- migrations/20161123020846_create_milestones.js
+++ migrations/20161123020846_create_milestones.js
@@ -4,12 +4,14 @@ exports.up = function(knex, Promise) {
     table.increments('id');
     table.string('Description');
     table.date('date_achieved');
+    table.integer('famous_person_id'); //Need to create the foreign id before officially linking it in the next line
+    table.foreign('famous_person_id').references('famous_people.id');
   })
 ])
 };
 
 exports.down = (knex, Promise) => {
- return Promise.all([
+  return Promise.all([
   knex.schema.dropTable('milestones')
-])
+  ])
 };
